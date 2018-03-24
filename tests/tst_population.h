e6aafd9d3a0c63f7802454f9ebce2ce3968df3b2
Michał Świętek <mswietku@gmail.com>
2018-03-24 22:31:39 +0100

    Test size and values of a population

--- tests/tst_population.h
+++ tests/tst_population.h
@@ -7,6 +7,31 @@ using namespace ga;
 
 TEST(Population, Size)
 {
-    EXPECT_EQ(1, 1);
-    ASSERT_THAT(0, Eq(0));
+    auto pop = Population{15,3,5};
+    EXPECT_EQ(pop.size(), 15) << "Number of individuals is not set properly";
+    for(const auto & ind : pop)
+    {
+        ASSERT_EQ(ind.size(), 3) << "Number of chromosomes is not set properly";
+        for(const auto & chrom : ind)
+        {
+            ASSERT_EQ(chrom.size(), 5) << "Number of bits is not set properly";
+        }
+    }
+
+}
+
+TEST(Population, Value)
+{
+    auto pop = Population{15,3,5};
+    for(const auto & ind : pop)
+    {
+        for(const auto & chrom : ind)
+        {
+            for(auto bit : chrom)
+            {
+                ASSERT_TRUE( bit==0 || bit==1 ) << "Wrong value of a bit";
+            }
+        }
+    }
+
 }
